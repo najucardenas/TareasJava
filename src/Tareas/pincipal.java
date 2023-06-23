7c715918360e566f6b1f7782e16431e56ed11919
najucardenas <103268071+najucardenas@users.noreply.github.com>
2023-06-22 20:27:10 -0600

    tarea 4

--- src/Tareas/pincipal.java
+++ src/Tareas/pincipal.java
@@ -8,10 +8,38 @@ public class pincipal {
 		//ProductoTresNumeros tres=new ProductoTresNumeros();
 		//tres.ProductoTresNumeros1();
 		
-	   DividirNumeros div=new DividirNumeros();
-		div.DividirNum();
+	  /* DividirNumeros div=new DividirNumeros();
+		div.DividirNum();*/
 		
 		//Ecuaciones ec=new Ecuaciones();
+		
+		///Inicia calculadora con numeros flotantes unsando clases
+		
+		/*Menu menu=new Menu();
+		menu.menu();*/
+		
+		//fin de calculadora  con  numeros flotantes usando clases
+		
+		
+		/// inicio sistema para calcular el interes simple y compuesto
+		/*Interes in=new Interes();
+		in.pedirDatos();
+        in.simple();
+		in.compuesto();*/
+		/// fin de interes simple y compuesto
+		
+		
+		//Inicia area de un circulo con valores de punto flotante
+		
+		/*AreaCirculoP area=new AreaCirculoP();
+		area.PedirDato();*/
+		
+		//fin area de un circulo con valores de punto flotante
+		
+		// Inica promedio de numeros de punto flotante
+		Promedio pro=new Promedio();
+		
+		// termina promedio de numeros de punto flotante
 	}
 
 }
